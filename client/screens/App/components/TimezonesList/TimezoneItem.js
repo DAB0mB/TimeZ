32c57706058ad0f0918b6dfe04e4a5b957953266
DAB0mB <emanor6@gmail.com>
2018-02-16 01:50:25 +0800

    Add formatted local time for each timezone item

--- client/screens/App/components/TimezonesList/TimezoneItem.js
+++ client/screens/App/components/TimezonesList/TimezoneItem.js
@@ -5,26 +5,78 @@ import RemoveCircleIcon from 'material-ui/svg-icons/content/remove-circle';
 
 import style from './style.css';
 
-export const TimezoneItem = ({ timezone, onRemove }) => (
-  <tr className={style.item}>
-    <td className={style.label}>{timezone.name}</td>
-    <td className={style.label}>{timezone.city}</td>
-    <td className={style.label}>{timezone.diff}</td>
-    <td>
-      <IconButton className={style.removeButton} onClick={onRemove.bind(null, timezone._id)}>
-        <RemoveCircleIcon />
-      </IconButton>
-    </td>
-  </tr>
-);
+const instances = [];
 
-TimezoneItem.propTypes = {
-  timezone: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    diff: PropTypes.string.isRequired,
-  }).isRequired,
-  onRemove: PropTypes.func.isRequired,
-};
+// Setting a global interval so all instances would be updated at the same time
+setInterval(() => {
+  instances.forEach(instance => instance.updateTime());
+}, 1000);
+
+class TimezoneItem extends React.Component {
+  static propTypes = {
+    timezone: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      diff: PropTypes.string.isRequired,
+    }).isRequired,
+    onRemove: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.numericDiff = Number(props.timezone.diff.substr(3));
+    this.state = {};
+
+    this.updateTime(true);
+  }
+
+  componentDidMount() {
+    // Will start auto-updating time
+    instances.push(this);
+  }
+
+  componentWillUnmount() {
+    // Will stop from auto-updating time
+    const index = instances.indexOf(this);
+    instances.splice(index, 1);
+  }
+
+  render() {
+    const { timezone, onRemove } = this.props;
+
+    return (
+      <tr className={style.item}>
+        <td className={style.label}>{timezone.name}</td>
+        <td className={style.label}>{timezone.city}</td>
+        <td className={style.label}>{timezone.diff}</td>
+        <td className={style.label}>{this.state.time}</td>
+        <td>
+          <IconButton className={style.removeButton} onClick={onRemove.bind(null, timezone._id)}>
+            <RemoveCircleIcon />
+          </IconButton>
+        </td>
+      </tr>
+    );
+  }
+
+  updateTime(initial) {
+    const geoDate = new Date();
+    const gmtDate = new Date(
+      geoDate.getTime() +
+      geoDate.getTimezoneOffset() * 60 * 1000 +
+      this.numericDiff * 60 * 60 * 1000
+    );
+
+    const time = `${gmtDate.getHours()}:${gmtDate.getMinutes()}:${gmtDate.getSeconds()}`;
+
+    if (initial) {
+      this.state.time = time;
+    }
+    else {
+      this.setState({ time });
+    }
+  }
+}
 
 export default TimezoneItem;
